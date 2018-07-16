cfee027bbd4c3587bfae452e57275d9e8d3c058c
jjorge <jjorge@dsic.upv.es>
2018-07-16 21:27:41 +0200

    run.sh updated

--- code/run.sh
+++ code/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export PETSC_DIR=/PATH/TO/PERSCDIR #i.e: export PETSC_DIR=/home/user/petsc-3.8.4
-export PETSC_ARCH=arch-used #i.e: #export PETSC_ARCH=arch-linux2-c-debug
-export SLEPC_DIR=/PATH/TO/PERSCDIR #i.e: export SLEPC_DIR=/home/user/slepc-3.8.3 
+#export PETSC_DIR=/PATH/TO/PERSCDIR #i.e: export PETSC_DIR=/home/user/petsc-3.8.4
+#export PETSC_ARCH=arch-used #i.e: #export PETSC_ARCH=arch-linux2-c-debug
+#export SLEPC_DIR=/PATH/TO/PERSCDIR #i.e: export SLEPC_DIR=/home/user/slepc-3.8.3 
 
 
 NUMPROCS=$1
@@ -10,7 +10,7 @@ NUMPROCS=$1
 #node01
 #node02
 #...
-#iface is the network interface that will be used to communicate with other nodes
-time mpirun -n $NUMPROCS -iface eth0 -f ~/hostfile python3 shell_matrix_example.py -eps_nev 5 -log_view -eps_view  -eps_max_it 4000 -eps_tol 1e-14 -eps_ncv 11 -eps_monitor_all
-
-
+#iface is the network interface that will be used to communicate with other nodes, 
+# you should change this according to your configuration
+#To get more information about the parameters, please visit: http://slepc.upv.es
+time mpirun -n $NUMPROCS -iface eth0 -f ~/hostfile python3 shell_matrix_example_phi2.py -eps_nev 5 -log_view -eps_view  -eps_max_it 100 -eps_tol 1e-14 -eps_ncv 11 -eps_monitor_all
